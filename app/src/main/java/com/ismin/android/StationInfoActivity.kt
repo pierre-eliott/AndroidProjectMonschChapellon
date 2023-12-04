de21872c8f9fd08c56f57fe56aa398870827b684
Pierre-Eliott <pierre-eliott.monsch@etu.emse.fr>
2023-12-04 13:20:47 +0100

    Modification StationInfoActivity & activity_station_info

--- app/src/main/java/com/ismin/android/StationInfoActivity.kt
+++ app/src/main/java/com/ismin/android/StationInfoActivity.kt
@@ -4,14 +4,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.TextView
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.MapsInitializer
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class StationInfoActivity : AppCompatActivity() {
+class StationInfoActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mapView: MapView
+    private var googleMap: GoogleMap? = null
+    private var station: Station? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_station_info)
 
+        mapView = findViewById(R.id.mapView)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+
         if (intent.hasExtra("station")) {
-            val station = intent.getSerializableExtra("station") as? Station
+            station = intent.getSerializableExtra("station") as? Station
 
             val addressTextView = findViewById<TextView>(R.id.id_info_adresse)
             val stationNameTextView = findViewById<TextView>(R.id.id_info_n_station)
@@ -41,6 +57,52 @@ class StationInfoActivity : AppCompatActivity() {
         val homeButton: ImageButton = findViewById(R.id.homeButton)
         homeButton.setOnClickListener {
             onBackPressed() // Retour à l'activité parente
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onMapReady(gMap: GoogleMap) {
+        googleMap = gMap
+
+        station?.let {
+            val latitude = it.ylatitude
+            val longitude = it.xlongitude
+            val stationLocation = LatLng(latitude, longitude)
+
+            if(it.isFavorite)
+            {
+                googleMap?.addMarker(MarkerOptions()
+                    .position(stationLocation)
+                    .title(it.n_station)
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)))
+            }
+            else{
+                googleMap?.addMarker(MarkerOptions()
+                    .position(stationLocation)
+                    .title(it.n_station)
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)))
+            }
+
+            googleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(stationLocation, 9f))
         }
     }
 }
